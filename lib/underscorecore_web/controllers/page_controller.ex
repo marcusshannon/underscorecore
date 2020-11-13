a20d6000f328c7df3f09469a4b9fd4490fbdf8b4
Marcus Shannon <mpshannon@me.com>
2020-11-12 23:24:51 -0500

    add root redirect

--- lib/underscorecore_web/controllers/page_controller.ex
+++ lib/underscorecore_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule UnderscorecoreWeb.PageController do
   use UnderscorecoreWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    redirect(conn, to: "/cores")
   end
 end
