b94cadadca19f592069fa0ca534365bc89c38b8d
Marcus Shannon <mpshannon@me.com>
2020-11-12 23:22:03 -0500

    change styles

---
+++ assets/tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [
+    '../lib/underscorecore_web/templates/**/*.html.eex',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/ui")],
+};
